01c1dc96dd2797630ff9772da7814b52e14ca5f0
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-01-15 22:15:01 +0530

    Add functional property

--- Vector/FindTheSecondLargestElement.cpp
+++ Vector/FindTheSecondLargestElement.cpp
@@ -1,6 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int findTheLargestElement(int array[], int size){
+    int largestElement = array[0];
+    for(int i=0; i<size; i++){
+        if(largestElement < array[i]){
+            largestElement = array[i];
+        }
+    }
+    return largestElement;
+}
+
 int main(){
     int array[] = {4,5,3,2,1,8};
     int size = sizeof(array)/sizeof(array[0]);
@@ -14,14 +24,9 @@ int main(){
     }
 
     array[index] = 0;
-    largest = array[0];
      
-    for(int i=0; i<size; i++){
-        if(largest < array[i]){
-            largest = array[i];
-        }
-    }
+    int large = findTheLargestElement(array, size);
 
-    cout<<"Second Largest: "<<largest;
+    cout<<"Second Largest: "<< large;
     
 }
