3a368df8001196289ec35298974c568625dad77b
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2025-01-31 19:05:13 +0530

    Update: Traverse Reverse array using Array

--- Pointers/ReverseTraverse.cpp
+++ Pointers/ReverseTraverse.cpp
@@ -6,8 +6,20 @@ int main(){
     int size = 5;
     int *ptr = array + size - 1;
     cout<<"Traverse the array in reverse using Pointer"<<endl;
+    // Using For Loop (Method 1)
     for(int i = size-1; i>=0; i--){
-        cout<<*(array + i);
+        cout<<*(array + i)<< " ";
+    }
+
+    cout<<endl;
+
+    //Using While Loop (Method 2)
+    while(1){
+        cout<<*ptr<<" ";
+        if(ptr == array){
+            break;
+        }
+        ptr--;
     }
     return 0;
 }
