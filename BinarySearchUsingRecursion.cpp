4cb806a12c748f1333960f3d86bd8c63d1f37d51
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-02-01 16:43:48 +0530

    binary search using recursion

---
+++ BinarySearchUsingRecursion.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+#include <vector>
+
+int binarySearch(vector<int> &input, int target, int low, int high){
+    if(low > high) return -1;
+    int mid = (low + high) / 2;
+    if(input[mid] == target) return mid;
+    if(input[mid] < target){
+        return binarySearch(input, target, mid+1, high);
+    }else{
+        return binarySearch(input, target, low, mid-1);
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> input;
+    for(int i=0; i<n; i++){
+        int x;
+        cin>>x;
+        input.push_back(x);
+    }
+    int tagret;
+    cin>>tagret;
+    cout<<binarySearch(input, tagret, 0 ,n-1)<<endl;
+    return 0;
+}
