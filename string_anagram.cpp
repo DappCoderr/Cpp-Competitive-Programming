172812d7d9cb32b2e9e475c2e663301b5fc84fa5
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-02-03 22:00:14 +0530

    string anagram

---
+++ string_anagram.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+// int main(){
+//     string str = "star";
+//     sort(str.begin(), str.end());
+//     cout<<str<<endl;
+// }
+
+string stringAnagram(string &str){
+    vector<int> freq(26,0);
+
+    for(int i=0; i<str.size(); i++){
+        int index = str[i] - 'a';
+        freq[index]+=1;
+    }
+
+    int j=0;
+    for(int i=0; i<freq.size(); i++){
+        while(freq[i]--){
+            str[j++] = i + 'a';
+        }
+    }
+
+    return str;
+}
+
+int main(){
+    string str = "star";
+    cout<<stringAnagram(str)<<endl;
+    return 0;
+}
