5d8237b5dbf711e1b370c18076d8326eee3b0d01
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-02-15 20:52:22 +0530

    find sum of vectors using sets

---
+++ find_sum_of_common_numbers.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+#include<set>
+using namespace std;
+
+int main(){
+    vector<int> v1 = {1,1,1,2,3,5,4,5,6};
+    vector<int> v2 = {4,2,1,6};
+    set<int> s1;
+    int ans = 0;
+    for(auto ele: v1){
+        s1.insert(ele);
+    }
+    for(auto ele: v2){
+        if(s1.count(ele)){
+            ans += ele;
+        }
+    }
+    cout<<"Output: "<<ans<<endl;
+    return 0;
+}
