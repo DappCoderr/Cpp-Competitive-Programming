de10b8792a1b540899c70eaf8c3314157d616525
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-02-17 11:31:11 +0530

    Update all files

--- LeetCode/Problem2.cpp
+++ LeetCode/Problem2.cpp
@@ -1,39 +1,70 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
+
+// Define: singly-linked list.
+struct ListNode {
+    int val;
+    ListNode* next;
+    
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
 
 ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
         
-        ListNode* dummy = new ListNode(0);
-        ListNode* curr = dummy;
+    ListNode* dummy = new ListNode(0);
+    ListNode* curr = dummy;
         
-        int carry = 0;
+    int carry = 0;
         
-        while(l1 != NULL || l2 != NULL || carry != 0){
+    while(l1 != NULL || l2 != NULL || carry != 0){
             
-            int sum = carry;
+        int sum = carry;
             
-            if(l1 != NULL){
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            
-            if(l2 != NULL){
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            
-            carry = sum / 10;
-            
-            curr->next = new ListNode(sum % 10);
-            curr = curr->next;
+        if(l1 != NULL){
+            sum += l1->val;
+            l1 = l1->next;
         }
+            
+        if(l2 != NULL){
+            sum += l2->val;
+            l2 = l2->next;
+        }
+            
+        carry = sum / 10;
+            
+        curr->next = new ListNode(sum % 10);
+        curr = curr->next;
+    }
         
-        return dummy->next;
+    return dummy->next;
+}
+
+// Function to print linked list
+void printList(ListNode* head){
+    while(head != NULL){
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
 }
 
 int main(){
-    ListNode* l1, l2, res;
-    res = addTwoNumbers(l1.l2);
-    cout<<res<<endl;
+    // Creating first number: 342 (stored as 2->4->3)
+    ListNode* l1 = new ListNode(2);
+    l1->next = new ListNode(4);
+    l1->next->next = new ListNode(3);
+
+    // Creating second number: 465 (stored as 5->6->4)
+    ListNode* l2 = new ListNode(5);
+    l2->next = new ListNode(6);
+    l2->next->next = new ListNode(4);
+
+    ListNode* res = addTwoNumbers(l1, l2);
+
+    printList(res);  // Expected: 7 0 8
+
     return 0;
 }
--- Sets/find_sum_of_common_numbers.cpp
+++ Sets/find_sum_of_common_numbers.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
     vector<int> v1 = {1,1,1,2,3,5,4,5,6};
-    vector<int> v2 = {4,2,1,6};
+    vector<int> v2 = {4,6};
     set<int> s1;
     int ans = 0;
     for(auto ele: v1){
