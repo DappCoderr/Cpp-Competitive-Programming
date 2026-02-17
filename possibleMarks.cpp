f0ccb932c275ef1139ee7e7c6d12261c33ccd63c
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-02-17 11:30:27 +0530

    possible marks using sets

---
+++ possibleMarks.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<unordered_set>
+using namespace std;
+
+int main(){
+    int p=1,q=-1,n;
+    cout<<"Enter Number of Questions: "<<endl;
+    cin>>n;
+
+    unordered_set<int> s;
+
+    for(int i=0; i<=n; i++){
+        for(int j=0; j<=n;j++){
+
+            int correct = i;
+            int incorrect = j;
+            int unattended = n - (i+j);
+
+            if(unattended>=0){
+                int score = correct*p + incorrect*q;
+                s.insert(score);
+            }else{
+                break;
+            }
+        }
+    }
+    
+    for(auto ele:s){
+        cout<<ele<<" ";
+    }
+    cout<<endl;
+    cout<<"Ans: "<<s.size()<<endl;
+    return 0;
+}
+
