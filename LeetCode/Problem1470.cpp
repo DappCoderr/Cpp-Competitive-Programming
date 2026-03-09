5aa5a90df0bedc409483ed7200839d67934c2eef
DappCoderr <68939930+DappCoderr@users.noreply.github.com>
2026-03-09 23:22:03 +0530

    add problem 1470 shuffle the array

---
+++ LeetCode/Problem1470.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+vector<int> shuffle(vector<int>& nums, int n) {
+    vector<int>result;
+    for(int i=0;i<n;i++){
+        result.push_back(nums[i]);
+        result.push_back(nums[n+i]);
+    }
+    return result;
+}
+
+int main(){
+    vector<int> nums = {1,2,3,4,5,6};
+    int n = 3;
+    shuffle(nums, n);
+    return 0;
+}
